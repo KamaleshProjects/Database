272a7086aeb90d9708656fc2b46ca477f40c5f3b
Kamalesh S <kamaleshs@Apples-MacBook-Pro.local>
2024-09-05 00:22:56 +0530

    read print execute read (repr) loop implementation, baby steps

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hey!, this is SQLWaffle!");
+
+    // repr -> read execute print loop
+    loop {
+        println!("> ");
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read command");
+        command = command.trim().to_string();
+        println!("You entered the command: '{}' ", command);
+    }
 }
